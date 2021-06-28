236d6d9e8ff147c5e3f992675f6e6a4e4ea62607
TaraMKelly <tara.kelly16@gmail.com>
2021-06-28 17:10:12 -0400

    commit tree 41a4fb7abccf4e3faec2e9388439b19172ebd534

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(funThing="roller-skate") {
+    return `This Saturday, I want to ${funThing}!`;
+}
+saturdayFun();
+
+const mondayWork = function(thingToDo="go to the office") {
+    return `This Monday, I will ${thingToDo}.`;
+}
+
+const wrapAdjective = function(someFlair="*") {
+    return function (anAdjective="special") {
+        return `You are ${someFlair}${anAdjective}${someFlair}!`;
+    }
+}
